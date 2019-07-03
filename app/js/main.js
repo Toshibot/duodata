ef597936224ceeac58f3f8bcfc611fa3a01e7c9f
Toshi Stewart <toshi.stewart@gmail.com>
2019-07-03 17:40:48 +0200

    Added Duo Data

--- app/js/main.js
+++ app/js/main.js
@@ -12,8 +12,20 @@ function dataDuo() {
     $.getJSON('https://www.duolingo.com/2017-06-30/users?username=Tardesh&_=1562161200287', function(json){
 
         var course = json.users[0].courses[0];
+        var course_name = course.title;
+        var crowns = course.crowns;
+        var xp = course.xp;
 
-        console.log(course);
+        var crowns_goal = 120*5;
+        var crowns_complete = (crowns/crowns_goal)*100;
+
+        console.log(json);
+
+        $('duo').append(
+            '<h2>Duo ' + course_name + '</h2>' +
+            '<progress max="100" value="' + crowns_complete + '"></progress>' +
+            '<p>' + Math.round(crowns_complete) + '% complete</p>'
+        );
     })
 }
 
