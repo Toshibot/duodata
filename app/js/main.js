421a3d39f6a66925d5e4cc0c24a3c235af3ee178
Toshi Stewart <toshi.stewart@gmail.com>
2019-07-04 10:48:19 +0200

    Changed get method

--- app/js/main.js
+++ app/js/main.js
@@ -28,30 +28,51 @@ function dataDuo() {
     //     );
     // })
 
-    var self = this;
+    // var self = this;
 
-    self.ajax = function(uri, method, data) {
-        var request = {
-           url: uri,
-           type: method,
-           accepts: "application/json",
-           cache: false,
-           dataType: "json",
-           data: JSON.stringify(data),
-           headers: {"Access-Control-Allow-Origin": "https://toshibot.github.io/duodata" },
-           error: function (jqXHR) {
-                 console.log("ajax error " + jqXHR.status);
-           }
+    // self.ajax = function(uri, method, data) {
+    //     var request = {
+    //        url: uri,
+    //        type: method,
+    //        accepts: "application/json",
+    //        cache: false,
+    //        dataType: "json",
+    //        data: JSON.stringify(data),
+    //        headers: {"Access-Control-Allow-Origin": "https://toshibot.github.io/duodata" },
+    //        error: function (jqXHR) {
+    //              console.log("ajax error " + jqXHR.status);
+    //        }
   
-        };
+    //     };
   
-        return $.ajax(request);
-    }
+    //     return $.ajax(request);
+    // }
 
-    self.tasksURI = "https://www.duolingo.com/2017-06-30/users/118369997?fields=courses";
+    // self.tasksURI = "https://www.duolingo.com/2017-06-30/users/118369997?fields=courses";
 
-    self.ajax(self.tasksURI, 'GET').done(function(data) {
+    // self.ajax(self.tasksURI, 'GET').done(function(data) {
+    //     console.log(data);
+    //     var course = data.courses[0];
+    //     var course_name = course.title;
+    //     var crowns = course.crowns;
+    //     var xp = course.xp;
+
+    //     var crowns_goal = 120*5;
+    //     var crowns_complete = (crowns/crowns_goal)*100;
+
+    //     $('duo').append(
+    //         '<h2>Duo ' + course_name + '</h2>' +
+    //         '<progress max="100" value="' + crowns_complete + '"></progress>' +
+    //         '<p>' + Math.round(crowns_complete) + '% complete</p>'
+    //     );
+    // })
+
+
+    const url = 'https://www.duolingo.com/2017-06-30/users/118369997?fields=courses';
+
+    $.get(url, function(data, status){
         console.log(data);
+
         var course = data.courses[0];
         var course_name = course.title;
         var crowns = course.crowns;
